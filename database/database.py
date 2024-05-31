8e68f5a8e2c0b0fd8d2a7a9e241b3bf1c69a7bb8
АринаКурманаева <@arinakurmanaeva0@gmail.com>
2024-05-31 13:27:52 +0300

    sec

--- database/database.py
+++ database/database.py
@@ -1,40 +1,37 @@
 import sqlite3
-
-def initialize_db(user_id, username, first_name, last_name):
-    conn = sqlite3. connect('bot_database.dp')
+def initialize_db():
+    conn = sqlite3.connect('bot_database.dp')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT
-            user_id INTEGER NOT NULL,
-            username TEXT,
-            first_name TEXT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            user_id INTEGER NOT NULL, 
+            username TEXT, 
+            first_name TEXT, 
             last_name TEXT
         )
     ''')
+
     conn.commit()
     conn.close()
 
-def initialize_db(user_id, username, first_name, last_name):
-    conn = sqlite3. connect('bot_database.dp')
+def add_user(user_id, username, first_name, last_name):
+    conn = sqlite3.connect('bot_database.dp')
     cursor = conn.cursor()
     cursor.execute('''
-        INSERT INTO user (user_id, username, first_name, last_name)
-        VALUS (?, ?, ?)
-    ''',(user_id, username, first_name, last_name)
+    INSERT INTO users (user_id, username, first_name, last_name)
+    VALUES (?, ?, ?, ?)
+    ''', (user_id, username, first_name, last_name))
+
     conn.commit()
-    conn.close()
+    conn.cursor()
 
 def get_user(user_id):
     conn = sqlite3.connect('bot_database.dp')
     cursor = conn.cursor()
     cursor.execute('''
-        SELECT * FROM users WHERE user_id = ?
-    ''', (user_id,))
-    conn.commit()
-    conn.cursor()
-    return user_id
-
-def add_user(user_id, username, firs_name, last_name):
-    conn = sqlite3.connect('bot_database.dp')
-    cursor = conn.cursor
+    SELECT * FROM users WHERE user_id = ?
+    ''',(user_id,))
+    user = cursor.fetchone()
+    conn.close()
+    return user
--- main.py
+++ main.py
@@ -7,21 +7,23 @@ bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
 initialize_db()
-g
 
 @dp.message_handler(commands= 'start')
 async def start(message: types.Message):
     user = get_user(message.from_user.id)
-    await message.answer('Привет', reply_markup= get_keyboard_1())
+    if user is None:
+        add_user(message.from_user.id, message.from_user.username, message.from_user.first_name, message.from_user.last_name)
+        await message.answer('Привет', reply_markup= get_keyboard_1())
+    else:
+        await message.answer('Привет', reply_markup= get_keyboard_1())
 
 
 @dp.message_handler(lambda message: message.text == 'отправь фото кота')
 async def button_1_click(message: types.Message):
-    await bot.send_photo(message.chat.id, photo= 'https://vetson.ru/upload/iblock/1bd/c73x437pcxc6afyjij70rduo6rw7g0rh.png', caption= 'Вот тебе кот!', reply_markup= get_keyboard_inline())
-
+    await bot.send_photo(message.chat.id, photo= 'https://vetson.ru/upload/iblock/1bd/c73x437pcxc6afyjij70rduo6rw7g0rh.png', caption= 'Вот тебе кот!')
 @dp.message_handler(lambda message: message.text == 'перейти на следующую клавиатуру')
 async def button_2_click(message: types.Message):
-    await message.answer('тут ты можешь попросить бота отправить фото собаки', reply_markup= get_keyboard_2())
+    await message.answer('тут ты можешь попросить бота отправить фото собаки')
 
 @dp.message_handler(lambda message: message.text == 'отправь фото собаки')
 async def button_3_click(message: types.Message):
